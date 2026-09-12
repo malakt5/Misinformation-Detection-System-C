05d47396746d15dca06d478fd8dfa93fa1338459
Malak Touibi <malaktouibi4@gmail.com>
2026-09-12 21:28:26 +0100

    Create fakesdb.h

---
+++ fakesdb.h
@@ -0,0 +1,17 @@
+#ifndef FAKESDB_H_INCLUDED
+#define FAKESDB_H_INCLUDED
+#define NB_FAKES 6
+#define NB_SUSPECTS 8
+
+static const char *BASE_FAKES[] = {
+"ils vous cachent", "partagez avant suppression",
+"ce qu on vous cache", "la verite sur",
+"les medias ne disent pas","wake up"
+};
+static const char *MOTS_SUSPECTS[] = {
+"alerte", "urgent", "exclusif", "censure",
+"complot", "secret", "interdit", "choc"
+};
+
+
+#endif
